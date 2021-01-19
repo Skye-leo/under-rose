83d1c8832ca52c80109cba2a3eeac666bc8fe9e2
heimdall <liuhaoyang@mzdata.top>
2021-01-19 16:27:07 +0800

    增加了断线重连

--- src/main/java/com/heim/underrose/config/WebSocketConfig.java
+++ src/main/java/com/heim/underrose/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.heim.underrose.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -13,6 +14,9 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 //注解开启使用STOMP协议来传输基于代理(message broker)的消息,这时控制器支持使用@MessageMapping,就像使用@RequestMapping一样
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    private static long HEART_BEAT=3000;
+
     //注册STOMP协议的节点(endpoint),并映射指定的url
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -22,8 +26,12 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     //配置消息代理(Message Broker)
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
+        te.setPoolSize(1);
+        te.setThreadNamePrefix("wss-heartbeat-thread-");
+        te.initialize();
         //点对点应配置一个/user消息代理，广播式应配置一个/topic消息代理,群发（mass），单独聊天（alone）
-        registry.enableSimpleBroker("/topic","/user","/mass","/alone");
+        registry.enableSimpleBroker("/topic","/user","/mass","/alone").setHeartbeatValue(new long[]{HEART_BEAT,HEART_BEAT}).setTaskScheduler(te);
         //点对点使用的订阅前缀（客户端订阅路径上会体现出来），不设置的话，默认也是/user/
         registry.setUserDestinationPrefix("/user");
     }
